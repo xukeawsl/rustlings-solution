1b3da0b35812cb2180e1bd1ac7411a6dc91d2411
xukeawsl <xukeawsl@gmail.com>
2023-11-30 12:22:46 +0000

    first commit

---
+++ clippy/target/debug/deps/clippy3-3675f093c48f2a7f.d
@@ -0,0 +1,9 @@
+/root/rustlings/exercises/clippy/target/debug/deps/clippy3-3675f093c48f2a7f.rmeta: clippy3.rs Cargo.toml
+
+/root/rustlings/exercises/clippy/target/debug/deps/clippy3-3675f093c48f2a7f.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
---
+++ tests/tests4.rs
@@ -0,0 +1,58 @@
+// tests4.rs
+//
+// Make sure that we're testing for the correct conditions!
+//
+// Execute `rustlings hint tests4` or use the `hint` watch subcommand for a
+// hint.
+
+
+
+struct Rectangle {
+    width: i32,
+    height: i32
+}
+
+impl Rectangle {
+    // Only change the test functions themselves
+    pub fn new(width: i32, height: i32) -> Self {
+        if width <= 0 || height <= 0 {
+            panic!("Rectangle width and height cannot be negative!")
+        }
+        Rectangle {width, height}
+    }
+
+    pub fn width(&self) -> i32 {
+        self.width
+    }
+
+    pub fn height(&self) -> i32 {
+        self.height
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn correct_width_and_height() {
+        // This test should check if the rectangle is the size that we pass into its constructor
+        let rect = Rectangle::new(10, 20);
+        assert_eq!(rect.width(), 10); // check width
+        assert_eq!(rect.height(), 20); // check height
+    }
+
+    #[test]
+    #[should_panic]
+    fn negative_width() {
+        // This test should check if program panics when we try to create rectangle with negative width
+        let _rect = Rectangle::new(-10, 10);
+    }
+
+    #[test]
+    #[should_panic]
+    fn negative_height() {
+        // This test should check if program panics when we try to create rectangle with negative height
+        let _rect = Rectangle::new(10, -10);
+    }
+}
